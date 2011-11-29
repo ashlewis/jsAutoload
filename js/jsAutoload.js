9a8862ca5653c6f92765465c50f6ea2b3d99e126
Ashley Lewis <ashley@thelostboy.co.uk>
2011-11-29 13:43:53 +0000

    swapped out local jquery for remote

--- js/jsAutoload.js
+++ js/jsAutoload.js
@@ -6,18 +6,17 @@ var AutoLoad = new function(){
 		try	{	
 			var autoloadNs =  eval(namespace)();			
 		}
-		catch(e if e instanceof ReferenceError){
-			
-			//var ns = e.message.split(' ', 1);
-		
-			$.ajax({
-				  url: 'js/'+ namespace +'.js',
-				  dataType: 'script',
-				  async: false,
-				  success: function(){						
-					autoloadNs = eval(namespace)();			
-				  }				  
-			});			
+		catch(e){
+			if (e instanceof ReferenceError) {
+				$.ajax({
+					  url: 'js/'+ namespace +'.js',
+					  dataType: 'script',
+					  async: false,
+					  success: function(){						
+						autoloadNs = eval(namespace)();			
+					  }				  
+				});
+			}
 		}		
 		return autoloadNs;		
 	};	
